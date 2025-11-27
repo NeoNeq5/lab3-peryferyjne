1b8c89ed94e9757f61507f12c8a5eed605fd5c70
jyugo0 <karolinamilka66@gmail.com>
2025-11-27 14:40:30 +0100

    Implement CountControlBit method in lab3.cs
    Added CountControlBit method to calculate control bit for a given code.

--- lab3.cs
+++ lab3.cs
@@ -6,6 +6,23 @@ class Program
     {
 
     }
+    static string CountControlBit (string code)
+    {
+        int sum = 0;
+        for (int i=0; i< code.Length; i++){
+
+            int n= int.Parse(code[i].ToString());
+            if ( (i+1)%2==0){
+               sum+=n*3;
+            } else {
+               sum+=n;
+            }
+         }
+        int controlN = sum % 10;
+        string controlS = controlN.ToString();
+        string fullCode= code + controlS;
+        return fullCode;
+    }
 
 
     static void WyswietlanieKodu(String kod){
